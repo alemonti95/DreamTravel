a1053d3358f5150d906c6e72bdcb107a94d5e482
alemonti95 <alessandro.monti1995@gmail.com>
2022-01-02 02:22:10 +0100

    modifica

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
-let firstDot = document.querySelector(".dot:nth-child(1)");
-let secondDot = document.querySelector(".dot:nth-child(2)");
-let thirdDot = document.querySelector(".dot:nth-child(3)");
-let container = document.getElementById("container");
+const firstDot = document.querySelector(".dot:nth-child(1)");
+const secondDot = document.querySelector(".dot:nth-child(2)");
+const thirdDot = document.querySelector(".dot:nth-child(3)");
+const container = document.getElementById("container");
 const hamburgerMenu = document.getElementById("burger-menu");
 const hamburgerMenu2 = document.getElementById("burger-menu-2");
-
-let counter = 0;
+const lateralMenu = document.getElementById("lateral-menu");
+const mainTitle = document.getElementById("main-title");
 
 /* Codice per hamburger-menu */
 hamburgerMenu.addEventListener("click", () => {
 
-    document.getElementById("lateral-menu").style.display = "flex";
+    lateralMenu.style.display = "flex";
     hamburgerMenu.style.display = "none";
     hamburgerMenu2.style.display = "block";
+    mainTitle.style.display = "none";
 })
 
 hamburgerMenu2.addEventListener("click", () => {
 
-    document.getElementById("lateral-menu").style.display = "none";
+    lateralMenu.style.display = "none";
     hamburgerMenu2.style.display = "none";
     hamburgerMenu.style.display = "block";
+    mainTitle.style.display = "block";
 })
