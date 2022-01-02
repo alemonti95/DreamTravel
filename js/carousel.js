fd0d8db8a02400b4c2ad163312806ad2b63569f3
alemonti95 <alessandro.monti1995@gmail.com>
2022-01-02 02:44:09 +0100

    mod

---
+++ js/carousel.js
@@ -0,0 +1,5 @@
+const dot = document.getElementsByClassName("dot");
+const firstDot = document.querySelectorAll(".dot:nth-child(1)");
+const secondDot = document.querySelectorAll(".dot:nth-child(2)");
+const thirdDot = document.querySelectorAll(".dot:nth-child(3)");
+
