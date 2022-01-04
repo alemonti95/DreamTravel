8f103c00ba2f082652cc88be1115e93101461e31
alemonti95 <alessandro.monti1995@gmail.com>
2022-01-04 05:06:02 +0100

    modifica

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,27 @@
-const dot = document.getElementsByClassName("dot");
-const firstDot = document.querySelectorAll(".dot:nth-child(1)");
-const secondDot = document.querySelectorAll(".dot:nth-child(2)");
-const thirdDot = document.querySelectorAll(".dot:nth-child(3)");
+const dotOne = document.getElementsByClassName("dot");
+const dotTwo = document.querySelectorAll(".dot:nth-child(2)");
+const dotThree =  document.querySelectorAll(".dot:nth-child(3)");
 
+const container2 = document.getElementById("#book-button");
+const a = document.getElementById("book-button");
+
+function dotClick()
+{
+    var element = 2;
+    container.style.background = "red";
+    if(element===1)
+    {
+        container.style.background = "none";
+        a.style.background = "red";
+    }
+    if(element===2)
+    {
+        container.style.background = "red";
+    }
+}
+
+dotOne[0].addEventListener("click", dotClick);
+
+dotOne[1].addEventListener("click", dotClick);
+
+dotOne[2].addEventListener("click", dotClick);
