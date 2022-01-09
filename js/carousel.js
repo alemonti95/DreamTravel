c4a4f51dcedc3b94cf0b641b1efa037447f8bf94
alemonti95 <alessandro.monti1995@gmail.com>
2022-01-09 05:02:40 +0100

    modifica carousel

--- js/carousel.js
+++ js/carousel.js
@@ -5,23 +5,30 @@ const dotThree =  document.querySelectorAll(".dot:nth-child(3)");
 const container2 = document.getElementById("#book-button");
 const a = document.getElementById("book-button");
 
-function dotClick()
-{
-    var element = 2;
-    container.style.background = "red";
-    if(element===1)
-    {
-        container.style.background = "none";
-        a.style.background = "red";
-    }
-    if(element===2)
+
+
+function changeBackgroundImage(){
+    if(this === dotOne[0].matches)
     {
         container.style.background = "red";
+        container.style.backgroundSize = "cover";
     }
+    console.log(this);
 }
 
-dotOne[0].addEventListener("click", dotClick);
 
-dotOne[1].addEventListener("click", dotClick);
+dotOne[0].addEventListener("click", () =>{
+    container.style.background = "linear-gradient(180deg, rgba(9, 69, 95, 0) 0%, #09455F 140%), url(../media/pexels-photo-1031200.jpeg)";
+    container.style.backgroundSize = "cover";
+});
 
-dotOne[2].addEventListener("click", dotClick);
+dotOne[1].addEventListener("click", () =>{
+    container.style.background = "linear-gradient(180deg, rgba(9, 69, 95, 0) 0%, #09455F 140%), url(../media/image_8.png)";
+    container.style.backgroundSize = "cover";
+});
+
+dotOne[2].addEventListener("click", () =>{
+    container.style.background = "linear-gradient(180deg, rgba(9, 69, 95, 0) 0%, #09455F 140%), url(../media/image9.jpg)";
+    container.style.backgroundSize = "cover";
+});
+
