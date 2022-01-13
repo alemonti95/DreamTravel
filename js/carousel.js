d45103dcf9e4534ea949b8daabd15c59e193f079
alemonti95 <alessandro.monti1995@gmail.com>
2022-01-13 23:02:06 +0100

    mod

--- js/carousel.js
+++ js/carousel.js
@@ -11,24 +11,28 @@ function changeBackgroundImage(){
     if(this === dotOne[0].matches)
     {
         container.style.background = "red";
-        container.style.backgroundSize = "cover";
+        
     }
     console.log(this);
 }
 
 
 dotOne[0].addEventListener("click", () =>{
-    container.style.background = "linear-gradient(180deg, rgba(9, 69, 95, 0) 0%, #09455F 140%), url(media/pexels-photo-1031200.jpeg)";
+    container.style.background =  "linear-gradient(180deg, rgba(9, 69, 95, 0) 0%, #09455F 140%), url(../media/pexels-photo-1031200.jpeg) no-repeat center center fixed";
     container.style.backgroundSize = "cover";
+    container.style.backgroundAttachment = "fixed";
 });
 
+
 dotOne[1].addEventListener("click", () =>{
-    container.style.background = "linear-gradient(180deg, rgba(9, 69, 95, 0) 0%, #09455F 140%), url(media/image_8.png)";
+    container.style.background =  "linear-gradient(180deg, rgba(9, 69, 95, 0) 0%, #09455F 140%), url(media/image_8.png) no-repeat center center fixed";
     container.style.backgroundSize = "cover";
+    container.style.backgroundAttachment = "fixed";
 });
 
 dotOne[2].addEventListener("click", () =>{
-    container.style.background = "linear-gradient(180deg, rgba(9, 69, 95, 0) 0%, #09455F 140%), url(media/image9.jpg)";
+    container.style.background =  "linear-gradient(180deg, rgba(9, 69, 95, 0) 0%, #09455F 140%), url(media/image9.jpg) no-repeat center center fixed";
     container.style.backgroundSize = "cover";
+    container.style.backgroundAttachment = "fixed";
 });
 
